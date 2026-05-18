e707ec954dc4d3d5a239fcad204838baba1d7e4a
Joel Jude <joel@Joels-MacBook-Pro.local>
2026-05-17 21:19:47 -0400

    Created environment variables for security

---
+++ scripts/test_connection.py
@@ -0,0 +1,16 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+print("Connected successfully!")
+conn.close()
